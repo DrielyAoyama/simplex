c5830ee81b00af3a36c7ac157da28acd78c87fdc
Driely Aoyama <driely.aoyama@hotmail.com>
2015-10-22 01:11:49 -0200

    limites e sombras

--- sensibilidade.php
+++ sensibilidade.php
@@ -81,13 +81,90 @@ $conteudo=$conteudo.'<p><strong>Isto é :</strong> refere-se a quantidade que o 
 $tabelaprecosombra=array();
 $aux=0;
 
+$recursos=array();
+$resultadorecurso=array();
 for ($coluna=0; $coluna < $qtdecolunas ; $coluna++) { 
-   if(trim(strtoupper(substr($tabelainicial[0][$coluna],0,1)))=='F'){
-       $tabelaprecosombra[$aux][0]=$tabelainicial[0][$coluna];
-       $tabelaprecosombra[$aux][1]=$tabelainicial[1][$_SESSION['qtdelinhas']];
+   if(trim(strtoupper(substr($tabelafinal[0][$coluna],0,1)))=='F'){
+       $tabelaprecosombra[$aux][0]=$tabelafinal[0][$coluna];
+       array_push($recursos, $tabelafinal[0][$coluna]);
+       $tabelaprecosombra[$aux][1]=$tabelafinal[$_SESSION['qtdelinhas']-1][$coluna];
+        array_push($resultadorecurso, $tabelafinal[$_SESSION['qtdelinhas']-1][$coluna]);
        $aux++;
    }
 }
+
+
+$deltas = array();
+$lmin = array();
+$lmax = array();
+$minima=9999;
+$maxima=-9999;
+$aux = 0;
+$aux2= 1;
+
+//calculando e imprimindo o limite mino e maximo
+for ($coluna=($_SESSION['qtdevariaveis']+1); $coluna < ($_SESSION['qtdecolunas']-1) ; $coluna++) { 
+  $minima=9999;
+  $maxima=-9999;
+    for ($linha=1; $linha < ($_SESSION['qtdelinhas']-1); $linha++) {  
+     $b = ($tabelafinal[$linha][$_SESSION['qtdecolunas']-1]);
+     $f =  ($tabelafinal[$linha][$coluna]);       
+     $vlrb = $tabelainicial[$aux2][$_SESSION['qtdecolunas']-1]; 
+      if ((($tabelafinal[$linha][$_SESSION['qtdecolunas']-1])!=0)  and  (($tabelafinal[$linha][$coluna])!=0)){
+        $deltas[$linha-1][$aux]   =   (round(((($b*-1)/$f)),2)+$vlrb);
+      //  $conteudo=$conteudo.($b*-1).' /  '.$f.' = '. (round(((($b*-1)/$f)),2)+$vlrb).'<br>';
+        ///// DELTA = ((B)*-1)/F
+      }else{
+         $deltas[$linha-1][$aux]=0+$vlrb; 
+      }
+
+      if( $deltas[$linha-1][$aux] < $minima){
+         $minima=$deltas[$linha-1][$aux];
+      }
+      if( $deltas[$linha-1][$aux] > $maxima){
+         $maxima=$deltas[$linha-1][$aux];
+      }
+    }
+    $lmax[$aux]=$maxima;
+    $lmin[$aux]=$minima;
+    $aux++;
+    $aux2++;
+}
+
+
+$conteudo=$conteudo.'<h3><strong>Limites</strong></h1><br>';
+$conteudo=$conteudo.'<p>Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto </p>';
+$conteudo=$conteudo.'<p>bla bla bla bla bla bla bla bla bla bla</p><br><br>';
+
+
+
+
+
+
+//escrevendo os limites minimos
+$aux=0;
+$aux2= 1;
+for ($coluna=($_SESSION['qtdevariaveis']+1); $coluna < ($_SESSION['qtdecolunas']-1) ; $coluna++) {
+  $conteudo=$conteudo.'<strong>'.$tabelafinal[0][$coluna].'</strong><br>'; 
+    for ($linha=1; $linha < ($_SESSION['qtdelinhas']-1); $linha++) {   
+      if (  ($tabelafinal[$linha][$_SESSION['qtdecolunas']-1])!=0  and   ($tabelafinal[$linha][$coluna]) !=0){
+        $b = ($tabelafinal[$linha][$_SESSION['qtdecolunas']-1]);
+        $f =  ($tabelafinal[$linha][$coluna]);
+        $vlrb = $tabelainicial[$aux2][$_SESSION['qtdecolunas']-1]; 
+        $conteudo=$conteudo.($b*-1).' /  '.$f.' + '.$vlrb.' = '.  (round(((($b*-1)/$f)),2)+$vlrb).'<br>';
+      }      
+    }
+   
+    $conteudo=$conteudo.'<strong>Minimo :'.$lmin[$aux].'</strong><br>';
+    $conteudo=$conteudo.'<strong>Máximo :'.$lmax[$aux].'</strong><br>';
+    $conteudo=$conteudo.'<br><hr>';
+    $aux++;    
+    $aux2++;
+}
+
+    
+
+
 
 
 $conteudo=$conteudo.'<div class="row"><div class="col-lg-12">';
@@ -96,6 +173,10 @@ $conteudo=$conteudo.'<table class="table table-bordered">
 
 $conteudo=$conteudo.'<th>Recursos</th>';
 $conteudo=$conteudo.'<th>Preço Sombra</th>';
+$conteudo=$conteudo.'<th>Limite Mínimo</th>';
+$conteudo=$conteudo.'<th>Limite Máximo</th>';
+$conteudo=$conteudo.'<th>Valor Inicial (B)</th>';
+
 $conteudo=$conteudo.'</tr></thead>';  
 
 for ($l=0; $l <= 2 ; $l++) {
@@ -105,14 +186,22 @@ for ($l=0; $l <= 2 ; $l++) {
         $conteudo=$conteudo.'<td>'.$tabelaprecosombra[$l][$c].'</td>';
       }
      }
-     $conteudo=$conteudo.'</tr>';
 }
+
+
+
+
+
 $conteudo=$conteudo.'</tbody>';
 $conteudo=$conteudo.'</table>';
 $conteudo=$conteudo.'</div>';
 
 
 $conteudo=$conteudo.'<br><br>';
+
+
+
+
 
 
 $tela->SetContent($conteudo);
